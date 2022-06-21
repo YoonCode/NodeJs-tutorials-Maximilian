cca4e93fc5f3f4322e272cb0f2768c341ed925fd
YoonCode <yjin830201@gmail.com>
2022-06-21 23:14:58 +0900

    install body-parser, understand how to get text from form

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', (req, res, next) => {
   console.log('This always runs!')
@@ -8,12 +11,17 @@ app.use('/', (req, res, next) => {
 })
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In the middleware!')
-  res.send('<h1>The "Add Product" Page!</h1>')
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>',
+  )
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!')
   res.send('<h1>Hello from express!</h1>')
 })
 
