bc516a8c0cf901027134cc8253612363ec22c7cd
YoonCode <yjin830201@gmail.com>
2022-06-25 15:18:10 +0900

    edit/delete product data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,21 +1,21 @@
 const Product = require('../models/product')
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/',
-    })
-  })
-}
-
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
+    })
+  })
+}
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
     })
   })
 }
