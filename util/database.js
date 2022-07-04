4cadeaae624142d74635f120f0e5ab09e6f45712
YoonCode <yjin830201@gmail.com>
2022-07-05 00:44:37 +0900

    install Sequelize, reset product model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', '@Tkdwls0201', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: '@Tkdwls0201',
 })
 
-module.exports = pool.promise()
+module.exports = sequelize
